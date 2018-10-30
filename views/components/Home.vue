e726dabf2991bd7ca1c2268ac6ab0642b76312ce
Brandon Moran <BrandonEM1@comcast.net>
2018-10-30 12:02:31 -0700

    content and stuff

--- views/components/Home.vue
+++ views/components/Home.vue
@@ -3,9 +3,31 @@
     <div class="row1">
       <div class="physical">
         <h1>Physical</h1>
+        <div id="morphing"> <svg width="140" height="140" viewBox="0 0 140 140">
+      <g fill="none" fill-rule="evenodd">
+        <g fill="currentColor" fill-opacity=".15" transform="translate(0 6)">
+          <polygon points="70 0 136.574 48.369 111.145 126.631 28.855 126.631 3.426 48.369"></polygon>
+          <polygon points="70 18 119.455 53.931 100.565 112.069 39.435 112.069 20.545 53.931"></polygon>
+          <polygon points="70 34.86 101.727 57.911 89.609 95.209 50.391 95.209 38.273 57.911"></polygon>
+          <polygon points="70 50.898 84.864 61.697 79.186 79.171 60.814 79.171 55.136 61.697"></polygon>
+        </g>
+        <polygon class="polymorph" stroke-width="1" stroke="currentColor" points="70 27.18219862061589 119.38681184584613 60.18181184584613 102.20406969569262 120.43882231230813 52.72688154153875 98.44880137938411 6.608198620615889 55.49212892492326 "></polygon>
+      </g>
+    </svg> </div>  
       </div>
       <div class="mental">
         <h1>Mental</h1>
+        <div id="morphing"> <svg width="140" height="140" viewBox="0 0 140 140">
+      <g fill="none" fill-rule="evenodd">
+        <g fill="currentColor" fill-opacity=".15" transform="translate(0 6)">
+          <polygon points="70 0 136.574 48.369 111.145 126.631 28.855 126.631 3.426 48.369"></polygon>
+          <polygon points="70 18 119.455 53.931 100.565 112.069 39.435 112.069 20.545 53.931"></polygon>
+          <polygon points="70 34.86 101.727 57.911 89.609 95.209 50.391 95.209 38.273 57.911"></polygon>
+          <polygon points="70 50.898 84.864 61.697 79.186 79.171 60.814 79.171 55.136 61.697"></polygon>
+        </g>
+        <polygon class="polymorph" stroke-width="1" stroke="currentColor" points="70 27.18219862061589 119.38681184584613 60.18181184584613 102.20406969569262 120.43882231230813 52.72688154153875 98.44880137938411 6.608198620615889 55.49212892492326 "></polygon>
+      </g>
+    </svg> </div>
       </div>
     </div>
     <div class="row2">
@@ -18,6 +40,20 @@
 
 
 <script>
+import anime from 'animejs'
+
+const morphing = anime({
+  targets: '#morphing .polymorph',
+  points: [
+    { value: '70 41 118.574 59.369 111.145 132.631 60.855 84.631 20.426 60.369' },
+    { value: '70 6 119.574 60.369 100.145 117.631 39.855 117.631 55.426 68.369' },
+    { value: '70 57 136.574 54.369 89.145 100.631 28.855 132.631 38.426 64.369' },
+    { value: '70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369' }
+  ],
+  easing: 'easeOutQuad',
+  duration: 2000,
+  loop: true
+});
 export default {
   name: 'Home',
   props: ['title']
--- views/components/Physical.vue
+++ views/components/Physical.vue
@@ -1,6 +1,118 @@
 <template>
   <div class="physical-container">
 
+<div class="row"> 
+  <div class="col-sm-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">STRETCH PRACTICES</h5>
+        <p class="card-text">LStanding Hamstring Stretch - Stand tall with your feet hip-width apart, knees slightly bent, arms by your sides. 
+          Exhale as you bend forward at the hips, lowering your head toward floor, while keeping your head, neck and shoulders relaxed. 
+          Wrap your arms around backs of your legs and hold anywhere from 45 seconds to two minutes. Bend your knees and roll up when you're done.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">STRETCH PRACTICES</h5>
+        <p class="card-text">DTriceps Stretch - Kneel, sit, or stand tall with feet hip-width apart, arms extended overhead. Bend your right elbow and 
+          reach your right hand to touch the top middle of your back. Reach your left hand overhead and grasp just below your right elbow. Gently pull 
+          your right elbow down and toward your head. Switch arms and repeat.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">STRETCH PRACTICES</h5>
+        <p class="card-text">Butterfly Stretch - Sit tall on the floor with the soles of your feet together, knees bent out to sides. Hold onto your ankles or feet, 
+          engage your abs, and slowly lower your body toward your feet as far as you can while pressing your knees toward the floor. If you're too tight to bend over, 
+          simply press your knees down. Hold this stretch for 30 seconds to 2 minutes.</p>
+      </div>
+    </div>
+  </div>
+    <div class="col-sm-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">STRETCH PRACTICES</h5>
+        <p class="card-text"> Lunging Hip Flexor Stretch - Kneel on your left knee. Place your right foot flat on the floor in front of you, knee bent. Lean forward, 
+          stretching your left hip toward the floor. Squeeze your butt; this will allow you to stretch your hip flexor even more. Hold for 30 seconds to 2 minutes. 
+          Switch sides and repeat.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row"> 
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">CARDIO EXERCISES</h5>
+        <p class="card-text">Power Walk - The point of speed walking is to flex and contract those leg muscles by walking at a fast 
+          rate of speed while also avoiding having both feet off the ground like you would when running or jogging. In this exercise, 
+          you sould also utilize your arms and sway them opposite your left and right feet. This excersices puts less strain on 
+          your knees as it would for jogging.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">CARDIO EXERCISES</h5>
+        <p class="card-text">Jogging Continuously - Be sure to stretch before and after. Attempt to start running at a medium 
+          pace so that you can sustain your energy for a longer period of time. Start jogging and track to see how long you can last. 
+          After you figure that out you can calculate that if you can last for 20min continuously, challenge yourself to jog out for 
+          10min and jog back in 10min or less. Push yourself at a further distance to increase your time.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">CARDIO EXERCISES</h5>
+        <p class="card-text">Running - Self explainitory. Be sure to stretch before and after. Whether it be for long distance or short 
+          bursts, remember to utilize most of your leg muscles to propell you forward more effectively</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row"> 
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">MUSCLE TONING EXERCISES</h5>
+        <p class="card-text">Bicycle Kicks - This abdominal exercise requires you to lay on your back with your arms on your sides.
+           Lift up both legs and simulate riding a bike by kicking out with one leg and pulling your other leg toward you. As a bonus, 
+           try locking your fingers behind your head and touch your right elbow to your left knee and vice versa. As an added bonus, 
+           hold a weight above your belly and alternate between bicycle kicks, then pausing to touch the weight on the ground your right and left side.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">MUSCLE TONING EXERCISES</h5>
+        <p class="card-text">Push Ups - Start by getting on your belly and push yourself off the ground as many times as you can. The trick behind gaining 
+          muscle is not to push yourself off the ground rapidly for quick pumps, rather you should slowly push yourself off the ground and contracting 
+          you muscle groups. As a bonus, apply some wights or get a chair to put your feet on.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">MUSCLE TONING EXERCISES</h5>
+        <p class="card-text">Squats - This activity will not only help improve muscle tone to your legs but will help when lifting things off the ground 
+          in a better posture which will avoid the use of your lower back muscles. If you are attending a gym or want to work out from home, it is 
+          important to stretch before and after. While doing this exercise, remember to focus on contracting your leg muscle groups for a better result.
+           Avoid squating with too much weight or else your leg muscles will be stressed which will not get you results.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
   </div>
 </template>
 
