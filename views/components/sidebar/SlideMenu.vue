a29f4d3c5418961865faa463c52781618b19d5e0
Elijah Kotyluk <elijah@elijahkotyluk.com>
2018-10-29 22:01:27 -0700

    Add SlideMenu component. Import SlideMenu component in App.vue.

---
+++ views/components/sidebar/SlideMenu.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="slideout"
+         :class="{ isOpen: isOpen }">
+      <div class="slideout-opener"
+           @click="toggle">{{openerText}}</div>
+      <ul class="slideout-menu">
+        <li class="slideout-menu-item"
+        	v-for="item in menu">{{item}}</li>
+        <li class="slideout-menu-item--small"
+        	v-for="item in smallMenu">{{item}}</li>
+      </ul>
+    </div>
+</template>
+
+
+<script>
+export default {
+  name: 'SlideMenu',
+  data () {
+    return {
+      openerText: '>',
+      isOpen: false,
+      menu: [ 'Home', 'Mental', 'Physical' ],
+    }
+  },
+  methods: {
+    open() {
+      this.openerText = '<';
+      this.isOpen = true;
+    },
+    close() {
+      this.openerText = '>';
+      this.isOpen = false;
+    },
+    toggle() {
+      if (this.isOpen) {
+        this.close();
+      } else {
+        this.open();
+      }
+    }
+  }
+}
+</script>
+
+
+<style lang="scss">
+.info {
+  position: absolute;
+  top: 50%; left: 50%;
+  transform: translateX(-50%);
+  text-align: center;
+}
+
+.title {
+  font-size: 24px;
+  font-weight: 600;
+}
+
+.description {
+  margin-top: 20px;
+}
+
+/*
+Our Zap Slideout styles...
+ */
+.slideout {
+  position: relative;
+  width: 240px;
+  height: 100vh;
+  padding: 30px;
+  background-color: rgba(200, 200, 200, 0.24);
+  border: 5px solid rgba(32, 32, 32, 1);
+  border-radius: 1.25em;
+  transform: translateX(-100%);
+  transition: transform 0.6s ease(out-cubic);
+
+  &.isOpen {
+    transform: translateX(0);
+  }
+}
+
+.slideout-opener {
+  position: absolute;
+  top: 20px;
+  left: 100%;
+  margin-left: 20px;
+  font-weight: 600;
+  text-transform: uppercase;
+  letter-spacing: 0.08em;
+  color: #fff;
+  cursor: pointer;
+
+  &:hover {
+    text-decoration: underline;
+  }
+}
+
+.slideout-menu {
+  font-weight: 600;
+  color: #fff;
+}
+
+.slideout-menu-item,
+.slideout-menu-item--small {
+  cursor: pointer;
+
+  &:hover {
+    text-decoration: underline;
+  }
+
+  & + & {
+    margin-top: 20px;
+  }
+}
+
+.slideout-menu-item {
+  font-size: 36px;
+
+  & + .slideout-menu-item--small {
+    margin-top: 30px;
+  }
+}
+
+.slideout-menu-item--small {
+  font-size: 18px;
+  font-weight: normal;
+  text-transform: uppercase;
+  letter-spacing: 0.08em;
+  color: #ecf0f1;
+}
+
+/* The famed Zap agency logo (TM) */
+.emoji {
+  height: 120px;
+}
+</style>
--- views/router/index.js
+++ views/router/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './components/Home.vue'
-import Mental from './components/Mental.vue'
-import Physical from './components/Physical.vue'
+import Home from '../components/Home.vue'
+import Mental from '../components/Mental.vue'
+import Physical from '../components/Physical.vue'
 
 Vue.use(Router)
 
