f149a1310b333f671ef300b71a88627bd8f4e457
Elijah Kotyluk <elijah@elijahkotyluk.com>
2018-10-30 17:28:13 -0700

    Add listActivities method for dynamically displaying activities.

--- views/components/Challenges.vue
+++ views/components/Challenges.vue
@@ -1,17 +1,40 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <div class="list-group">
+      <div class="list-group-item" v-for="activity in activities">
+
+      </div>
+    </div>
+  </div>
 </template>
 
 
 <script>
-  export default {
-    name: 'Challenges',
-    data () {
-      return {
-        activities: []
-      }
+import { http } from '../config/http.js'
+
+export default {
+  name: 'Challenges',
+  data () {
+    return {
+      activities: []
+    }
+  },
+  created: function() {
+    this.listActivities()
+  },
+  methods: {
+    listActivities() {
+      http
+      .get("activities")
+      .then(response => {
+        this.activities = response.data.activities;
+      })
+      .catch(e => {
+        this.errors.push(e);
+      });
     }
   }
+}
 </script>
 
 
